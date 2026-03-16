05b4a1444aab1d94fcc767cd6b8cc40088452e7b
nedeadinside <aquvva67@gmail.com>
2026-03-17 01:36:56 +0700

    Added camera and barecode requests

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -26,8 +26,14 @@ dependencies {
     implementation(project(":domain"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Room — реальная локальная база данных (SQLite)
+    // локальная база данных (SQLite)
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+
+    // для штрихов
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
 }
